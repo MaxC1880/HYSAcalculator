45c4e351c0c22d62868150230ade28f4d29d70a3
Maxwell Chin <109707857+MaxC1880@users.noreply.github.com>
2023-06-22 23:24:43 -0500

    Add files via upload

---
+++ HYSAcalculator.py
@@ -0,0 +1,37 @@
+'''
+This project is a GUI calculator for a high yield savings account. 
+The GUI will display 4 input boxes. An intial deposit, monthly deposit, APY yield, and years to calculate
+The result will be a number at the end of the year, as well as a graph that displays the growth of the account.
+Possible extras could include a bar graph or just numbers that display how much of the final amount was the initial, monthly deposit,
+or interest earned.
+'''
+
+
+
+
+def calculator(initial, monthly, APY, years):
+    print('Initial deposit: ' + initial)
+    print('Monthly contribution: ' + monthly)
+    print('APY: ' + APY)
+    print('Years calcuting: ' + years)
+
+
+def main():
+
+    while True:
+        try:
+            initial = float (input ('Initial deposit: '))
+            monthly = float (input ('Monthly Contribution: '))
+            apy = float (input ('APY: '))
+            years = float (input ('Years: '))
+            break
+            
+
+        except(ValueError):
+            print('Wrong')
+        
+
+    calculator(initial, monthly, apy, years)
+
+
+main()
