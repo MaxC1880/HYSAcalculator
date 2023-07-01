247a82bf45b9a0688a5fd3381674eaf6bd6d874c
Maxwell <maximum49er@gmail.com>
2023-07-01 18:14:55 -0500

    Changed a bunch of things

--- HYSAcalculator.py
+++ HYSAcalculator.py
@@ -5,46 +5,47 @@ The result will be a number at the end of the year, as well as a graph that disp
 Possible extras could include a bar graph or just numbers that display how much of the final amount was the initial, monthly deposit,
 or interest earned.
 '''
-
-
-
+import tkinter
 
 def calculator(initial, monthly, APY, years):
-    total_bal = 0
     apy_ratio = APY / 100
-    monthly_contribute = monthly * (years * 12)
+    total_monthly = (monthly * 12) * years
+    total_months = int((years * 12))
+    count = years
 
     contribution_interest = 0
-    months_count = (years * 12)
 
-    for i in (0, (years * 12)):
-        contribution_interest += (monthly * apy_ratio * months_count)
-        months_count -= 1
+    for i in range (0, total_months):
+        contribution_interest += (monthly * apy_ratio * count)
     
-    total_bal = initial  + monthly_contribute + contribution_interest
+    total_bal = initial  + total_monthly + contribution_interest
     printResults(initial, monthly, APY, years, total_bal)
+
 
 
 def printResults(initial, monthly, APY, years, total_bal):
     print('\nYour initial deposit was: $' + str(initial) + '\nYour monthly contributions were: $' + str(monthly) + '\nYour APY was: ' + str(APY) + '%' +  
           '\nYour years looking to calcualte was: ' + str(years) + ' years \nYour total balance in your savings after ' + str(years) + ' years is: $' + str(total_bal))
 
-def main():
-
+def get_positive_float_input(prompt):
     while True:
         try:
-            initial = float (input ('Initial deposit: '))
-            monthly = float (input ('Monthly Contribution: '))
-            apy = float (input ('APY: '))
-            years = int (input ('Years (in whole numebrs): '))
-            break
-            
+            value = float(input(prompt))
+            if value < 0:
+                print("Error, input cannot be less than 0")
+            else:
+                return value
+        except ValueError:
+            print("Invalid input, please enter a valid number")
 
-        except(ValueError):
-            print('Wrong input try again\n')
+initial = get_positive_float_input("Initial deposit: ")
+monthly = get_positive_float_input("Monthly contributions: ")
+apy = get_positive_float_input("APY percentage: ")
+years = get_positive_float_input("Years to calculate: ")
+
         
-
-    calculator(initial, monthly, apy, years)
-
+def main():
+    calculator(initial, monthly, apy, years)    
 
 main()
+
